3aab61335b92081bbe4853bd42c409a093af5859
Hannes Pieper <pieper@embedded.rwth-aachen.de>
2024-07-09 15:29:23 +0200

    fix: Rename StandardOptimizer to Standard for consistent naming

--- src/metrics/ThresholdOptimizer/Standard.py
+++ src/metrics/ThresholdOptimizer/Standard.py
@@ -2,6 +2,6 @@ from metrics import *
 from metrics.ThresholdOptimizer.IThresholdOptimizer import IThresholdOptimizer
 
 
-class StandardOptimizer(IThresholdOptimizer):
+class Standard(IThresholdOptimizer):
     def calculate_optimal_threshold(self, tpr: list[float], fpr: list[float], threshold: list[float]) -> float:
         return 0.5
