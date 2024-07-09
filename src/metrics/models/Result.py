dc01d69fe71f1fd28145300c2bdc2b9519405c62
Hannes Pieper <hannes.pieper@rwth-aachen.de>
2024-07-09 14:16:48 +0200

    fix: Missing level of config read

--- src/metrics/models/Result.py
+++ src/metrics/models/Result.py
@@ -11,11 +11,11 @@ class EvaluationInformation:
     def __init__(self, config, model, dataset_training=None, dataset_test=None):
 
         self.model = model
-        self.eval_storage_location = config['storage_location']  # TODO check what is correct
+        self.eval_storage_location = config['storage_path']  # TODO check what is correct
         self.dataset_training = dataset_training  # TODO replace by representation of dataset
         self.dataset_test = dataset_test  # TODO replace by representation of dataset
         self.model_name = model.name
-        self.eval_name = config['evaluation']['eval_name']
+        self.eval_name = config['evaluation']['evaluation_name']
         self.cross_validation_performed = config["process"]["perform_cross_validation"]
         self.evaluation_performed = config["process"]["calculate_evaluation_metrics"]
         if self.cross_validation_performed:
@@ -25,9 +25,9 @@ class EvaluationInformation:
             self.random_state = config["evaluation"]["cross_validation"]["random_state"]
         else:
             self.n_splits = 0
-
+        
         self.model_has_proba = model.has_predict_proba()
-        self.requested_metrics = config["evaluation_metrics"]
+        self.requested_metrics = config["evaluation"]["evaluation_metrics"]
         self.contained_metrics = []  # TODO check how to communicate incompatible metrics
         for metric in self.requested_metrics:
             if not eval(metric + "().needs_probabilities()"):
