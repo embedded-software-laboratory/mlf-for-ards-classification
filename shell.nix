5b0377bdb7aeedc6c7d9131e05537be2800da9dd
Alexander Baron <alexander.baron1@rwth-aachen.de>
2024-07-22 23:50:17 +0200

    fix(regression): nix shell

--- shell.nix
+++ shell.nix
@@ -13,9 +13,9 @@ let
       export MAMBA_ROOT_PREFIX=${builtins.getEnv "PWD"}/.mamba
 			export TMPDIR=${builtins.getEnv "PWD"}/.tmp
 			mkdir -p $TMPDIR
-      micromamba create -q -n ai
-      micromamba activate ai
-      micromamba install --yes -f environment_hpc.yml -c conda-forge
+      micromamba create -q -n mlp_framework
+      micromamba activate mlp_framework
+      micromamba install --yes -f src/environment_hpc.yml -c conda-forge
       set +e
     '';
   };
