21103b8692f8cef818529993610e15db4c467cd6
haenfry <haenfry@outlook.com>
2024-08-06 18:57:40 +0200

    debug: try out stuff

--- src/metrics/Metrics.py
+++ src/metrics/Metrics.py
@@ -10,10 +10,10 @@ from metrics.Models import *
 
 class Accuracy(FloatMetricSpec):
 
-    def calculate_metric(self, metric_params: dict) -> metrics.GenericMetric:
+    def calculate_metric(self, metric_params: dict) -> metrics.Models.GenericMetric:
         predicted_class = metric_params['predicted_label']
         true_class = metric_params['true_labels']
-        return metrics.GenericMetric(metric_name="Accuracy",
+        return metrics.Models.GenericMetric(metric_name="Accuracy",
                              metric_value=FloatValue(metric_value=accuracy_score(true_class, predicted_class)),
                              metric_spec=Accuracy())
 
@@ -22,12 +22,12 @@ class Accuracy(FloatMetricSpec):
 
 
 class AUC(FloatMetricSpec):
-    def calculate_metric(self, metric_parameters: dict) -> GenericMetric:
+    def calculate_metric(self, metric_parameters: dict) -> metrics.Models.GenericMetric:
         true_label = metric_parameters['true_labels']
         predicition_probs = metric_parameters['prediction_probs']
         fpr, tpr, _ = roc_curve(true_label, predicition_probs)
         auc_score = auc(fpr, tpr)
-        return GenericMetric(metric_name="AUC", metric_value=FloatValue(metric_value=auc_score), metric_spec=AUC())
+        return metrics.Models.GenericMetric(metric_name="AUC", metric_value=FloatValue(metric_value=auc_score), metric_spec=AUC())
 
     def needs_probabilities(self) -> bool:
         return True
@@ -35,10 +35,10 @@ class AUC(FloatMetricSpec):
 
 class F1Score(FloatMetricSpec):
 
-    def calculate_metric(self, metric_params: dict) -> GenericMetric:
+    def calculate_metric(self, metric_params: dict) -> metrics.Models.GenericMetric:
         predicted_class = metric_params['predicted_label']
         true_class = metric_params['true_labels']
-        return GenericMetric(metric_name="F1-Score",
+        return metrics.Models.GenericMetric(metric_name="F1-Score",
                              metric_value=FloatValue(metric_value=f1_score(predicted_class, true_class)),
                              metric_spec=F1Score())
 
@@ -47,11 +47,11 @@ class F1Score(FloatMetricSpec):
 
 
 class FPR(ListMetricSpec):
-    def calculate_metric(self, metric_parameters: dict) -> GenericMetric:
+    def calculate_metric(self, metric_parameters: dict) -> metrics.Models.GenericMetric:
         prediction_probs = metric_parameters['prediction_probs']
         true_labels = metric_parameters['true_labels']
         fpr, _, _ = roc_curve(true_labels, prediction_probs)
-        return GenericMetric(metric_name="FPR", metric_value=ListValue(metric_value=fpr), metric_spec=FPR())
+        return metrics.Models.GenericMetric(metric_name="FPR", metric_value=ListValue(metric_value=fpr), metric_spec=FPR())
 
     def needs_probabilities(self) -> bool:
         return True
@@ -59,10 +59,10 @@ class FPR(ListMetricSpec):
 
 class MCC(FloatMetricSpec):
 
-    def calculate_metric(self, metric_params: dict) -> GenericMetric:
+    def calculate_metric(self, metric_params: dict) -> metrics.Models.GenericMetric:
         predicted_class = metric_params['predicted_label']
         true_class = metric_params['true_labels']
-        return GenericMetric(metric_name="MCC",
+        return metrics.Models.GenericMetric(metric_name="MCC",
                              metric_value=FloatValue(metric_value=matthews_corrcoef(true_class, predicted_class)),
                              metric_spec=MCC())
 
@@ -71,13 +71,13 @@ class MCC(FloatMetricSpec):
 
 
 class OptimalProbability(FloatMetricSpec):
-    def calculate_metric(self, metric_parameters: dict) -> GenericMetric:
+    def calculate_metric(self, metric_parameters: dict) -> metrics.Models.GenericMetric:
         fpr = metric_parameters['fpr']
         tpr = metric_parameters['tpr']
         threshold = metric_parameters['thresholds']
         calc_func = metric_parameters['calc_func']
         optimal_prob = calc_func(fpr, tpr, threshold)
-        return GenericMetric(metric_name="Optimal Decision Threshold",
+        return metrics.Models.GenericMetric(metric_name="Optimal Decision Threshold",
                              metric_value=FloatValue(metric_value=optimal_prob),
                              metric_spec=OptimalProbability())
 
@@ -86,12 +86,12 @@ class OptimalProbability(FloatMetricSpec):
 
 
 class Sensitivity(FloatMetricSpec):
-    def calculate_metric(self, metric_parameters: dict) -> GenericMetric:
+    def calculate_metric(self, metric_parameters: dict) -> metrics.Models.GenericMetric:
         prediction_labels = metric_parameters['predicted_label']
         true_labels = metric_parameters['true_labels']
         tn, fp, fn, tp = confusion_matrix(true_labels, prediction_labels).ravel()
 
-        return GenericMetric(metric_name="Sensitivity",
+        return metrics.Models.GenericMetric(metric_name="Sensitivity",
                              metric_value=FloatValue(metric_value=(tp / (tp + fn))),
                              metric_spec=Sensitivity())
 
@@ -100,12 +100,12 @@ class Sensitivity(FloatMetricSpec):
 
 
 class Specificity(FloatMetricSpec):
-    def calculate_metric(self, metric_parameters: dict) -> GenericMetric:
+    def calculate_metric(self, metric_parameters: dict) -> metrics.Models.GenericMetric:
         prediction_labels = metric_parameters['predicted_label']
         true_labels = metric_parameters['true_labels']
         tn, fp, fn, tp = confusion_matrix(true_labels, prediction_labels).ravel()
 
-        return GenericMetric(metric_name="Specificity",
+        return metrics.Models.GenericMetric(metric_name="Specificity",
                              metric_value=FloatValue(metric_value=(tn / (tn + fp))),
                              metric_spec=Specificity())
 
@@ -114,11 +114,11 @@ class Specificity(FloatMetricSpec):
 
 
 class TPR(ListMetricSpec):
-    def calculate_metric(self, metric_parameters: dict) -> GenericMetric:
+    def calculate_metric(self, metric_parameters: dict) -> metrics.Models.GenericMetric:
         prediction_probs = metric_parameters['prediction_probs']
         true_labels = metric_parameters['true_labels']
         _, tpr, _ = roc_curve(true_labels, prediction_probs)
-        return GenericMetric(metric_name="TPR", metric_value=ListValue(metric_value=tpr), metric_spec=TPR())
+        return metrics.Models.GenericMetric(metric_name="TPR", metric_value=ListValue(metric_value=tpr), metric_spec=TPR())
 
     def needs_probabilities(self) -> bool:
         return True
