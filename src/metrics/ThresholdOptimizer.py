a459000b5df4570999820295884797df1eecaecf
Hannes Pieper <pieper@embedded.rwth-aachen.de>
2024-08-26 22:04:37 +0200

    debug: Added debug output for ThresholdOptimizer.py

--- src/metrics/ThresholdOptimizer.py
+++ src/metrics/ThresholdOptimizer.py
@@ -15,10 +15,17 @@ class Standard(IThresholdOptimizer):
 
 class MaxTPRMinFPR(IThresholdOptimizer):
     def calculate_optimal_threshold(self, tpr: ndarray, fpr: ndarray, threshold: ndarray) -> float:
-        print(fpr)
+        print("TPR")
         print(tpr)
+        print("FPR")
+        print(fpr)
+        print("Difference:")
+        print(tpr-fpr)
         print(np.argmax(tpr-fpr))
+        print("Threshold")
+        print(threshold)
         optimal_threshold = threshold[np.argmax(tpr - fpr)]
+        print(optimal_threshold)
         return optimal_threshold
 
 
