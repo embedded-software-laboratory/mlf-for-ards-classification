57455895b5ea0921d7cd59a1d0af9a9c8d1ae8c0
Hannes Pieper <pieper@embedded.rwth-aachen.de>
2024-12-06 16:38:01 +0100

    feat: Added loading/changed saving of timeseries models, refactored imports

--- src/ml_models/timeseries_model.py
+++ src/ml_models/timeseries_model.py
@@ -1,12 +1,14 @@
 
-import pydantic
 
 from ml_models.model_interface import Model
+from ml_models.ModelMetaData import ModelMetaDataFactory, ModelMetadata
 
-from evaluation.Evaluation import ModelEvaluation
-from ml_models.ModelMetaData import ModelMetaDataFactory
 from processing import TimeSeriesDataset
+from metrics import EvalResultFactory, EvalResult
 
+from evaluation import ModelEvaluation
+
+import json
 
 class TimeSeriesModel(Model):
 
@@ -15,6 +17,9 @@ class TimeSeriesModel(Model):
         self.type = 'TimeSeriesModel'
 
     def get_params(self):
+        raise NotImplementedError
+
+    def set_params(self, params: dict):
         raise NotImplementedError
 
     @property
@@ -34,36 +39,49 @@ class TimeSeriesModel(Model):
         labels = training_data["ards"]
         predictors = training_data.loc[:, training_data.columns != 'ards']
         model_evaluator.evaluate_timeseries_model(predictors, labels, stage, training_data_meta_data, training_data_meta_data, split_name)
+
         self.training_evaluation = model_evaluator.evaluation_results[stage]
 
 
-    def save(self, filepath):
+
+    def save(self, filepath: str, name: str= None):
         if not self.trained:
             print("It makes no sense to save the model before training it")
             return
-
-        evaluation_location = filepath + f"_{self.algorithm}_{self.name}_training_evaluation.json"
+        if not name:
+            base_path = filepath + f"{self.algorithm}_{self.name}"
+        else:
+            base_path = filepath + name
+        evaluation_location = base_path + "_training_evaluation.json"
         with open(evaluation_location, "w") as evaluation_file:
-            evaluation_file.write(self.training_evaluation.to_json(indent=4))
+            print(self.training_evaluation)
+            evaluation_file.write(self.training_evaluation.model_dump_json(indent=4))
 
         self.meta_data.ml_model_training_evaluation_location = evaluation_location
-        meta_data_path = filepath + f"_{self.algorithm}_{self.name}_meta_data.json"
+        meta_data_path = base_path + "_meta_data.json"
         with open(meta_data_path, "w", encoding="utf-8") as meta_data_file:
             meta_data_file.write(self.meta_data.model_dump_json(indent=4))
 
         self.save_model(filepath)
 
     def load(self, filepath):
-        # TODO finish
         self.load_model(filepath)
-        meta_data_path = filepath  + f"_{self.algorithm}_{self.name}_meta_data.json"
-        model_meta_data = pydantic.parse_raw(meta_data_path)
+        meta_data_path = filepath  + f"{self.algorithm}_{self.name}_meta_data.json"
+        with open(meta_data_path, 'r') as meta_data_file:
+            meta_data = json.load(meta_data_file)
+
+        model_meta_data = ModelMetadata(**meta_data)
         self.meta_data = model_meta_data
 
-        evaluation_location = filepath + f"_{self.algorithm}_{self.name}_training_evaluation.json"
-        model_training_evaluation = pydantic.parse_raw(evaluation_location)
+        evaluation_location = filepath + f"{self.algorithm}_{self.name}_training_evaluation.json"
+        with open(evaluation_location, 'r') as evaluation_file:
+            training_metrics = json.load(evaluation_file)
+
+        model_training_evaluation = EvalResult(**training_metrics)
+
+        self.trained = True
         self.training_evaluation = model_training_evaluation
-        pass
+
 
 
 class TimeSeriesProbaModel(TimeSeriesModel):
