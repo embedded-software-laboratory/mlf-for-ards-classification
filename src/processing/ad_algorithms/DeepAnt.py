a920bc263f2615254b2790593d831c5736468148
hannes.pieper <pieper@embeded.rwth-aachen.de>
2025-04-09 14:13:37 +0200

    feat: Added metadata, fixed concatination of datasets

--- src/processing/ad_algorithms/DeepAnt.py
+++ src/processing/ad_algorithms/DeepAnt.py
@@ -6,13 +6,14 @@ import pandas as pd
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
-from statsmodels.sandbox.regression.gmm import results_class_dict
+
 
 from tensorflow import keras
 
 from processing.ad_algorithms.AnomalyDetector import AnomalyDetector
 from processing.ad_algorithms.WindowGenerator import WindowGenerator
-
+from processing.datasets_metadata import AnomalyDetectionMetaData
+tf.config.threading.set_inter_op_parallelism_threads(8)
 class DeepAntPredictor:
 
     def __init__(self, output_dim: int=1, hidden_units: int = 256, max_epochs: int =20, name: str = "DeepAntPredictor"):
@@ -21,7 +22,9 @@ class DeepAntPredictor:
         self.hidden_units = hidden_units
         self.max_epochs = max_epochs
         self.model = None
+        self._build_model()
         self.trained = False
+
 
 
     def _build_model(self):
@@ -44,7 +47,7 @@ class DeepAntPredictor:
             verbose=1
         )
         checkpoint = keras.callbacks.ModelCheckpoint('best_model.h5', monitor='val_loss', verbose=1, save_best_only=True, mode='min')
-        self.model.compile(optimizer=keras.optimizers.Adam, loss=keras.losses.MSE, metrics=[keras.metrics.MAE])
+        self.model.compile(optimizer=keras.optimizers.Adam() , loss=keras.losses.MSE, metrics=[keras.metrics.MAE])
         history = self.model.fit(data_training, epochs=self.max_epochs, validation_data=data_validation, callbacks=[early_stopping_criteria, checkpoint])
         self.trained = True
         return history
@@ -57,6 +60,7 @@ class DeepAntAnomalyDetector(AnomalyDetector):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.model = {}
         self.deep_ant_output_dim = int(kwargs.get('output_dim', 1))
         self.deep_ant_hidden_units = int(kwargs.get('hidden_units', 256))
         self.deep_ant_max_epochs = int(kwargs.get('max_epochs', 20))
@@ -70,7 +74,19 @@ class DeepAntAnomalyDetector(AnomalyDetector):
         self.needs_full_data = True
         self.window_generator_config = dict(kwargs.get('window_generator_config', {}))
 
+    def create_meta_data(self):
+        meta_data_dict = super().create_meta_data()
+        meta_data_dict["anomaly_detection_algorithm"] = self.type
+        meta_data_dict["algorithm_specific_settings"] = {"length_prediction_horizon": self.deep_ant_output_dim,
+                                                         "hidden_units": self.deep_ant_hidden_units,
+                                                         "max_epochs": self.deep_ant_max_epochs,
+                                                         "val_percentage": self.val_percentage,
+                                                         "train_percentage": self.train_percentage,
+                                                         "test_percentage": self.test_percentage,
+                                                         "seed": self.seed,
+                                                         "window_generator_config": self.window_generator_config,}
 
+        return AnomalyDetectionMetaData(**meta_data_dict)
 
     def run(self,  dataframe_detection: pd.DataFrame, job_count: int, total_jobs: int) -> pd.DataFrame:
 
@@ -85,6 +101,7 @@ class DeepAntAnomalyDetector(AnomalyDetector):
                 else:
                     raise ValueError("Double label detected for label: " + label)
         for item in self.datasets_to_create:
+            print("Running dataset: ", item["name"])
             results.append(self._run_step(prepared_data, item))
         df_anomaly = pd.DataFrame()
         for item in results:
@@ -115,13 +132,14 @@ class DeepAntAnomalyDetector(AnomalyDetector):
         test_data = dataframe[dataframe["patient_id"].isin(test_patients)].reset_index()
         result_dict = {"train": train_data, "val": val_data, "test": test_data}
         if not self.datasets_to_create:
-            self.datasets_to_create = [{"name": column, "labels": column, "features": column} for column in
+            self.datasets_to_create = [{"name": column, "labels": [column], "features": [column]} for column in
                                        dataframe.columns if column not in self.columns_not_to_check]
         return result_dict
 
 
 
     def _run_step(self, data: dict[str, pd.DataFrame], dataset_to_create: dict[str, Union[list, str]]) -> pd.DataFrame:
+
         name = dataset_to_create["name"]
         train = data["train"]
         val = data["val"]
@@ -129,7 +147,7 @@ class DeepAntAnomalyDetector(AnomalyDetector):
         test_patients = test["patient_id"].unique().tolist()
         val_patients = val["patient_id"].unique().tolist()
         train_patients = train["patient_id"].unique().tolist()
-        relevant_columns = dataset_to_create["labels"] + dataset_to_create["features"]
+        relevant_columns = dataset_to_create["labels"] + dataset_to_create["features"] + ["patient_id", "time"]
         relevant_train = train[relevant_columns]
         relevant_train = relevant_train.dropna(how='any', axis=0).reset_index(drop=True)
         relevant_val = val[relevant_columns]
@@ -142,9 +160,15 @@ class DeepAntAnomalyDetector(AnomalyDetector):
 
         scaler = MinMaxScaler()
         scaler.fit(relevant_train)
-        relevant_train = scaler.transform(relevant_train)
-        relevant_val = scaler.transform(relevant_val)
-        relevant_test = scaler.transform(relevant_test)
+        scaled_train = scaler.transform(relevant_train)
+        scaled_val = scaler.transform(relevant_val)
+        scaled_test = scaler.transform(relevant_test)
+        relevant_train = pd.DataFrame(scaled_train, columns=relevant_train.columns, index=relevant_train.index)
+        relevant_val = pd.DataFrame(scaled_val, columns=relevant_val.columns, index=relevant_val.index)
+        relevant_test = pd.DataFrame(scaled_test, columns=relevant_test.columns, index=relevant_test.index)
+        relevant_train.drop(columns="patient_id", inplace=True)
+        relevant_val.drop(columns="patient_id", inplace=True)
+        relevant_test.drop(columns="patient_id", inplace=True)
         train_dataset_list = []
         val_dataset_list = []
         test_dataset_list = []
@@ -158,27 +182,45 @@ class DeepAntAnomalyDetector(AnomalyDetector):
             windowed_data = WindowGenerator(patient_val, **self.window_generator_config).generate_dataset()
             val_dataset_list.append(windowed_data)
 
+
+
+        train_dataset = None
+        for i in range(len(train_dataset_list)):
+            print(f"{i}/{len(train_dataset_list)}")
+            if train_dataset is None and train_dataset_list[i] is not None:
+                train_dataset = train_dataset_list[i]
+            elif train_dataset_list[i] is not None:
+                train_dataset = train_dataset.concatenate(train_dataset_list[i])
+
+
+        val_dataset = None
+        for i in range(len(val_dataset_list)):
+            print(f"{i}/{len(val_dataset_list)}")
+            if val_dataset is None and val_dataset_list[i] is not None:
+                val_dataset = val_dataset_list[i]
+            elif val_dataset_list[i] is not None:
+                val_dataset = val_dataset.concatenate(val_dataset_list[i])
+
+
+
+        model = DeepAntPredictor(output_dim=len(dataset_to_create["labels"]), hidden_units=self.deep_ant_hidden_units, max_epochs=self.deep_ant_max_epochs, name=name)
+        model, history = self._train_ad_model(train_dataset, val_dataset, model=model)
         for test_patients in patient_divisions_test.values():
             patient_test = relevant_test[test_patients[0]:test_patients[1]]
             windowed_data = WindowGenerator(patient_test, **self.window_generator_config).generate_dataset()
-            train_dataset_list.append(windowed_data)
+            test_dataset_list.append(windowed_data)
 
-        train_dataset = train_dataset_list[0]
-        for i in range(1, len(train_dataset_list)):
-            train_dataset.concatenate(train_dataset_list[i])
+        test_dataset = None
+        for i in range(len(test_dataset_list)):
+            if test_dataset is None and test_dataset_list[i] is not None:
+                test_dataset = test_dataset_list[i]
+                print(len(list(test_dataset.unbatch())))
+            elif test_dataset_list[i] is not None:
+                test_dataset = test_dataset.concatenate(test_dataset_list[i])
 
-        val_dataset = val_dataset_list[0]
-        for i in range(1, len(val_dataset_list)):
-            val_dataset.concatenate(val_dataset_list[i])
-
-        test_dataset = test_dataset_list[0]
-        for i in range(1, len(test_dataset_list)):
-            test_dataset.concatenate(test_dataset_list[i])
-
-        model  = DeepAntPredictor(output_dim=len(dataset_to_create["labels"]), hidden_units=self.deep_ant_hidden_units, max_epochs=self.deep_ant_max_epochs, name=name)
-        model, history = self._train_ad_model(train_dataset, val_dataset, model=model)
+        print(len(list(test_dataset.unbatch())))
         anomalies = self._predict(test_dataset, model=model)
-
+        self.model[name] = model
         for column in dataset_to_create["labels"]:
             test[column + "_anomaly"] = anomalies["results"]
         #if self.handling_strategy == "use_prediction":
@@ -199,8 +241,8 @@ class DeepAntAnomalyDetector(AnomalyDetector):
         patient_max_time_df = dataframe.groupby("patient_id")["time"].idxmax()
         patient_min_time_df = dataframe.groupby("patient_id")["time"].idxmin()
         for i in range(len(patient_ids)):
-            patient_dict[patient_ids[i]] = (int(patient_min_time_df[i]),
-                                            int(patient_max_time_df[i])
+            patient_dict[patient_ids[i]] = (int(patient_min_time_df.iloc[i]),
+                                            int(patient_max_time_df.iloc[i])
                                              )
         return patient_dict
 
@@ -229,10 +271,11 @@ class DeepAntAnomalyDetector(AnomalyDetector):
     def _predict(self, data, **kwargs) -> dict:
         model = kwargs['model']
         anomaly_dict = {"results": []}
+        print(len(list(data.unbatch())))
         if not model.trained:
             raise ValueError("Model must be trained before prediction")
         else:
-            predictions = self.model.predict(data)
+            predictions = model.predict(data)
             ground_truth = np.concatenate([y for x,y in data], axis=0).squeeze()
             anomaly_scores = np.linalg.norm(predictions - ground_truth, axis=1)
             threshold = self._calculate_threshold(anomaly_scores)
