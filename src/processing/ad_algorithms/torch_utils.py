bd8d820ecd0e2a2e89b84b0a4a34935f610c1da0
Hannes Pieper <pieper@embedded.rwth-aachen.de>
2025-04-25 18:45:37 +0200

    doc: Added Docstring

--- src/processing/ad_algorithms/torch_utils.py
+++ src/processing/ad_algorithms/torch_utils.py
@@ -40,7 +40,18 @@ def check_directory(directory: str):
         logger.info(f"Directory {directory} already exists.")
 
 def split_patients(dataframe: pd.DataFrame, anomaly_df: pd.DataFrame, patient_id: float) -> (pd.DataFrame, pd.DataFrame):
+    """
+        Splits the anomaly DataFrame and the DataFrame containing the original data for a specific patient_id from the rest of the data.
 
+        Args:
+            dataframe (pd.DataFrame): The original DataFrame containing the data.
+            anomaly_df (pd.DataFrame): The DataFrame containing the anomalies.
+            patient_id (float): The patient ID to filter the DataFrames by.
+
+        Returns:
+            tuple [pd.DataFrame, pd.DataFrame]: A tuple containing the DataFrame for the specific patient_id and the DataFrame with anomalies for that patient_id.
+
+    """
     patient_df = dataframe[dataframe["patient_id"] == patient_id].reset_index(drop=True)
     anomaly_df = anomaly_df[anomaly_df["patient_id"] == patient_id].reset_index(drop=True)
     return patient_df, anomaly_df
