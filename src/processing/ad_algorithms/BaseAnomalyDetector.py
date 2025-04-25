3c73d7ace3dd11abf857a259d25d0f9213406078
Hannes Pieper <pieper@embedded.rwth-aachen.de>
2025-04-25 18:55:37 +0200

    feat, refactor, doc: Added calculation of anomaly percentages for approaches that utilize full data, added doc strings, integrated _handle_anomalies_patient and _handle_anomalies, added _fix_anomaly_df, added _calculate_anomaly_count (per step and full), added saving of anomaly dfs

--- src/processing/ad_algorithms/BaseAnomalyDetector.py
+++ src/processing/ad_algorithms/BaseAnomalyDetector.py
@@ -1,3 +1,4 @@
+import pickle
 from multiprocessing import Pool
 
 import numpy as np
@@ -9,9 +10,13 @@ import pandas as pd
 
 
 class BaseAnomalyDetector:
+    # TODO: Make sure anomaly df is saved as one file
 
     def __init__(self, **kwargs):
-        self.can_predict = False
+        self.saved_anomaly_data = False
+        self.anomaly_data_dir = None
+        self.can_predict_value = False
+        self.patient_level_anomaly_detection = False
         self.name = None
         self.type = None
         self.model = None
@@ -23,7 +28,6 @@ class BaseAnomalyDetector:
         self.max_processes = 16
         self.needs_full_data = False
         self.anomaly_counts = None
-        # TODO add all static variables here
         self.columns_not_to_check = ["patient_id", "time", "ards", "chest-injury", "sepsis", "xray", "pneumonia", "pulmonary-edema", "hypervolemia", "heart-failure"]
         for key, value in kwargs.items():
             if key in self.__dict__.keys():
@@ -77,6 +81,16 @@ class BaseAnomalyDetector:
         else:
             dataframe = pd.concat(process_pool_data_list, ignore_index=True).reset_index(drop=True)
             fixed_df, anomaly_counts = self.run(dataframe, 0, 1)
+            finished_anomaly_count_dict = {}
+            for key, value in anomaly_counts.items():
+                total_anomalies_name = key + "_total_anomalies"
+                total_data_name = key + "_total_data"
+                percentage_anomalies_name = key + "_percentage_anomalies"
+                finished_anomaly_count_dict[key] = {
+                    total_anomalies_name: value["anomaly_count"],
+                    total_data_name: value["total_data"],
+                    percentage_anomalies_name: value["anomaly_count"] / value["total_data"]
+                }
             process_pool_data_list, n_jobs = prepare_multiprocessing(fixed_df, patients_per_process)
         dataframe = pd.concat(process_pool_data_list, ignore_index=True).reset_index(drop=True)
         self.anomaly_counts = anomaly_counts
@@ -101,21 +115,31 @@ class BaseAnomalyDetector:
 
     def fix_anomaly_data(self, data_to_fix: pd.DataFrame, detected_anomalies_file: str, detected_anomalies_meta_data_file: str) -> pd.DataFrame:
         """
-            Fixes anonmalies in the given Dataframe by utilizing already detected anomalies that have been saved on the disk.
+            Fixes anomalies in the given Dataframe by utilizing already detected anomalies that have been saved on the disk.
+
+            Args:
+                data_to_fix (pd.DataFrame): The input DataFrame containing the data with anomalies.
+                detected_anomalies_file (str): The file path to the file which contains the detected anomalies as well as patient ids and timestamps.
+                detected_anomalies_meta_data_file (str): The file path to the file which contains the meta data of the detected anomalies.
+
 
             Returns:
                 pd.DataFrame: The processed DataFrame with anomalies handled.
         """
+        # TODO read all files in the directory and merge them
         detected_anomalies_df = pd.read_pickle(detected_anomalies_file)
-        meta_data = pd.read_pickle(detected_anomalies_meta_data_file)
+        with open(detected_anomalies_meta_data_file, "rb") as f:
+            meta_data = pickle.load(f)
+
+
         relevant_data_to_fix = data_to_fix[data_to_fix["patient_id"].isin(meta_data["contained_patients"])]
 
 
         with Pool(processes=self.max_processes) as pool:
-            df_list = pool.starmap(split_patients, [(relevant_data_to_fix, patient_id) for patient_id in meta_data["contained_patients"]])
-        # TODO check if the arguments passed are correct
+            df_list = pool.starmap(split_patients, [(relevant_data_to_fix, detected_anomalies_df,  patient_id) for patient_id in meta_data["contained_patients"]])
+
         with Pool(processes=self.max_processes) as pool:
-            fixed_patients = pool.starmap(self._handle_anomalies_patient, [(detected_anomalies_df[1], patient_df[0], patient_df[0]) for patient_df in df_list])
+            fixed_patients = pool.starmap(self._handle_anomalies_patient, [(patient_df[1], patient_df[0][detected_anomalies_df.columns], patient_df[0]) for patient_df in df_list])
         fixed_df = pd.concat(fixed_patients, ignore_index=True).reset_index(drop=True)
 
 
@@ -123,7 +147,17 @@ class BaseAnomalyDetector:
 
 
     def _handle_anomalies_patient(self, anomaly_df: pd.DataFrame, relevant_data: pd.DataFrame, original_data: pd.DataFrame) -> pd.DataFrame:
+        """
+            This function is used to clean the anomalies in the data. It is applied for each patient separately.
 
+            Args:
+                anomaly_df (pd.DataFrame): The DataFrame containing the detected anomalies.
+                relevant_data (pd.DataFrame): The DataFrame containing the data to be fixed.
+                original_data (pd.DataFrame): The complete DataFrame containing the original data.
+
+            Returns:
+                pd.DataFrame: The complete DataFrame for a patient with anomalies handled.
+        """
 
         anomaly_df = self._fix_anomaly_df(anomaly_df, relevant_data)
         if self.handling_strategy == "delete_value":
@@ -134,9 +168,9 @@ class BaseAnomalyDetector:
             fixed_df = self._delete_row_if_any_anomaly(anomaly_df, relevant_data)
         elif self.handling_strategy == "delete_row_if_many_anomalies":
             fixed_df = self._delete_row_if_many_anomalies(anomaly_df, relevant_data)
-        elif self.handling_strategy == "use_prediction" and self.can_predict:
+        elif self.handling_strategy == "use_prediction" and self.can_predict_value:
             fixed_df = self._use_prediction(anomaly_df, relevant_data)
-        elif self.handling_strategy == "use_prediction" and not self.can_predict:
+        elif self.handling_strategy == "use_prediction" and not self.can_predict_value:
             raise NotImplementedError(f"Algorithm {self.type} does not support prediction as a handling strategy for anomalies.")
         else:
             raise ValueError(f"Unknown fixing strategy {self.handling_strategy}")
@@ -144,7 +178,21 @@ class BaseAnomalyDetector:
         finished_df.update(fixed_df)
         return finished_df
 
-    def _fix_anomaly_df(self, anomaly_df: pd.DataFrame, starting_data: pd.DataFrame) -> pd.DataFrame:
+    @staticmethod
+    def _fix_anomaly_df(anomaly_df: pd.DataFrame, starting_data: pd.DataFrame) -> pd.DataFrame:
+
+        """
+            This function processes the anomaly DataFrame to ensure it has an entry for each timestamp in the original patients data.
+            If there is no entry for a given timestamp, it is assumed that no anomaly can be presented at this time and therefore the value is set to False
+
+            Args:
+                anomaly_df (pd.DataFrame): The DataFrame containing the detected anomalies.
+                starting_data (pd.DataFrame): The DataFrame containing the data to be fixed.
+
+            Returns:
+                pd.DataFrame: The anomaly DataFrame having an entry for each timestamp and column in the original patients data.
+        """
+
         anomaly_df_fixed = anomaly_df.copy(deep=True)
         if len(anomaly_df_fixed.index) != len(starting_data.index):
             for index, row in starting_data.iterrows():
@@ -154,6 +202,83 @@ class BaseAnomalyDetector:
         anomaly_df_fixed.replace(np.nan, True, inplace=True)
         anomaly_df_fixed.drop(columns=["patient_id", "time"], inplace=True)
         return anomaly_df_fixed
+
+    def _calculate_anomaly_count(self, anomaly_df: pd.DataFrame, relevant_data: pd.DataFrame,
+                                 anomaly_count_dict: dict = None) -> dict[str, dict[str, int]]:
+        """
+            This function counts the number of anomalies in each column of the DataFrame and the total amount of data present in each column.
+
+            Args:
+                anomaly_df (pd.DataFrame): The DataFrame containing the detected anomalies.
+                relevant_data (pd.DataFrame): The DataFrame containing the values on which anomaly detection was done.
+                anomaly_count_dict (dict): A dictionary to store the counts of anomalies and total data for each column.
+
+            Returns:
+                dict: A dictionary containing the counts of anomalies and total data for each column.
+        """
+
+        relevant_columns = [column for column in anomaly_df.columns if column not in self.columns_not_to_check]
+        if self.patient_level_anomaly_detection:
+            anomaly_count_dict = self._calculate_anomaly_count_per_step(anomaly_df, relevant_data, relevant_columns, anomaly_count_dict)
+        else:
+            anomaly_count_dict = self._calculate_anomaly_count_full(anomaly_df, relevant_data, relevant_columns)
+        return anomaly_count_dict
+
+    @staticmethod
+    def _calculate_anomaly_count_per_step(anomaly_df: pd.DataFrame, relevant_data: pd.DataFrame, relevant_columns: list[str],
+                                                anomaly_count_dict: dict = None) -> dict[str, dict[str, int]]:
+
+
+        for column in relevant_columns:
+            value_count = relevant_data[column].count()
+            if column == "patient_id" or column == "time":
+                continue
+            anomaly_count = anomaly_df[column].sum()
+            if column in anomaly_count_dict.keys():
+                new_anomaly_count = anomaly_count_dict[column]["anomaly_count"] + anomaly_count
+                new_value_count = anomaly_count_dict[column]["total_data"] + value_count
+                anomaly_count_dict[column] = {"total_data": new_value_count,
+                                              "anomaly_count": new_anomaly_count}
+            else:
+                anomaly_count_dict[column] = {"total_data": value_count,
+                                              "anomaly_count": anomaly_count}
+        return anomaly_count_dict
+
+
+    @staticmethod
+    def _calculate_anomaly_count_full(anomaly_df: pd.DataFrame, relevant_data: pd.DataFrame, relevant_columns: list[str])\
+            -> dict[str, dict[str, int]]:
+        anomaly_count_dict = {}
+
+        for column in relevant_columns:
+            if column in relevant_data.columns:
+                total_value = relevant_data[column].count()
+                anomaly_count_dict[column] = {
+                    "anomaly_count": anomaly_df[column].sum(),
+                    "total_data": total_value
+                }
+
+        return anomaly_count_dict
+
+    def _save_anomaly_df(self, anomaly_df: pd.DataFrame) -> str:
+
+        """
+            Saves the anomaly DataFrame to a file.
+
+            Args:
+                anomaly_df (pd.DataFrame): The DataFrame containing the detected anomalies.
+
+            Returns:
+                str: The file path where the anomaly DataFrame is saved.
+        """
+        self.saved_anomaly_data = True
+        patient_ids = anomaly_df["patient_id"].unique().tolist()
+        first_patient_id = str(patient_ids[0]).replace(".", "_")
+        last_patient_id = str(patient_ids[-1]).replace(".", "_")
+        anomaly_df_path = f"{self.anomaly_data_dir}/anomaly_data_{self.name}_{first_patient_id}_{last_patient_id}.pkl"
+        with open(anomaly_df_path, "wb") as f:
+            pickle.dump(anomaly_df, f)
+        return anomaly_df_path
 
 
     def _train_ad_model(self, data_training, data_validation, **kwargs):
@@ -169,15 +294,51 @@ class BaseAnomalyDetector:
     def _prepare_data(self, dataframe: pd.DataFrame) -> dict:
         raise NotImplementedError()
 
-    def _handle_anomalies(self, anomalies: dict, anomalous_data : pd.DataFrame, original_data: pd.DataFrame) -> pd.DataFrame:
-        raise NotImplementedError()
+    def _handle_anomalies(self, detected_anomalies_df : pd.DataFrame, original_data: pd.DataFrame) -> pd.DataFrame:
+        if self.needs_full_data:
+            max_processes = self.max_processes
+        else:
+            max_processes = 1
+
+        anomaly_df_patients = detected_anomalies_df["patient_id"].unique().tolist()
+        with Pool(processes=max_processes) as pool:
+            patient_dfs = pool.starmap(split_patients, [(original_data, detected_anomalies_df, patient_id) for patient_id in anomaly_df_patients])
+
+        with Pool(processes=max_processes) as pool:
+            fixed_dfs = pool.starmap(self._handle_anomalies_patient, [(patient_df[1], patient_df[0][detected_anomalies_df.columns], patient_df[0]) for patient_df in patient_dfs])
+
+        fixed_df = pd.concat(fixed_dfs, ignore_index=True).reset_index(drop=True)
+        return fixed_df
 
     @staticmethod
     def _delete_value(anomaly_df: pd.DataFrame, dataframe: pd.DataFrame) -> pd.DataFrame:
+        """
+            This function handles anomalies by deleting the values in the patients DataFrame that are marked as anomalies.
+            It replaces the values with NaN.
+
+            Args:
+                anomaly_df (pd.DataFrame): The DataFrame containing the detected anomalies.
+                dataframe (pd.DataFrame): The DataFrame containing the data to be fixed.
+
+            Returns:
+                pd.DataFrame: The processed DataFrame with anomalies handled.
+        """
         dataframe = dataframe.mask(anomaly_df)
         return dataframe
 
     def _delete_than_impute(self, anomaly_df: pd.DataFrame, dataframe: pd.DataFrame) -> pd.DataFrame:
+        """
+            This function handles anomalies by deleting the values in the patients DataFrame that are marked as anomalies.
+            After deleting the values, it applies a fixing algorithm to fill the missing values. Values that can not be fixed are set to NaN.
+
+            Args:
+                anomaly_df (pd.DataFrame): The DataFrame containing the detected anomalies.
+                dataframe (pd.DataFrame): The DataFrame containing the data to be fixed.
+
+            Returns:
+                pd.DataFrame: The processed DataFrame with anomalies handled.
+        """
+
         dataframe = dataframe.fillna(-100000)
         dataframe = dataframe.mask(anomaly_df)
         dataframe = self._fix_deleted(dataframe)
@@ -186,17 +347,36 @@ class BaseAnomalyDetector:
 
     @staticmethod
     def _delete_row_if_any_anomaly(anomaly_df: pd.DataFrame, dataframe: pd.DataFrame) -> pd.DataFrame:
+        """
+            This function handles anomalies by removing the whole row from the patients DataFrame if any of the values in the row are marked as anomalies.
 
+            Args:
+                anomaly_df (pd.DataFrame): The DataFrame containing the detected anomalies.
+                dataframe (pd.DataFrame): The DataFrame containing the data to be fixed.
+
+            Returns:
+                pd.DataFrame: The processed DataFrame with anomalies handled.
+        """
         dataframe = dataframe.mask(anomaly_df)
         index_to_drop = []
         for index, row in dataframe.iterrows():
             if row.isnull().any():
                 index_to_drop.append(index)
-        dataframe = dataframe.drop(index_to_drop)
+        dataframe = dataframe.drop(index_to_drop).reset_index(drop=True)
         return dataframe
 
     def _delete_row_if_many_anomalies(self, anomaly_df: pd.DataFrame, dataframe: pd.DataFrame) -> pd.DataFrame:
+        """
+            This function handles anomalies by removing the whole row from the patients DataFrame if more than a certain percentage of the values in the row are marked as anomalies.
+            The percentage is defined by the class variable anomaly_threshold.
 
+            Args:
+                anomaly_df (pd.DataFrame): The DataFrame containing the detected anomalies.
+                dataframe (pd.DataFrame): The DataFrame containing the data to be fixed.
+
+            Returns:
+                pd.DataFrame: The processed DataFrame with anomalies handled.
+        """
         n_columns = len(dataframe.columns)
         n_anomalies = anomaly_df.sum(axis=1)
         dataframe = dataframe.fillna(-100000)
@@ -231,6 +411,8 @@ class BaseAnomalyDetector:
     def _use_prediction(self, prediction: pd.DataFrame, dataframe: pd.DataFrame) -> pd.DataFrame:
         raise NotImplementedError()
 
+
+
     def create_meta_data(self) -> dict:
         meta_data_dict = {
             "name": self.name,
@@ -245,6 +427,9 @@ class BaseAnomalyDetector:
             meta_data_dict["anomaly_threshold"] = self.anomaly_threshold
         else:
             meta_data_dict["anomaly_threshold"] = None
+        if self.saved_anomaly_data:
+            meta_data_dict["anomaly_data_dir"] = self.anomaly_data_dir
+
         return meta_data_dict
 
 
